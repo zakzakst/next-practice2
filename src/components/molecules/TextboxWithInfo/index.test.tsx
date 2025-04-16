fccaf228870def0327103b69b121f58f9bd584dd
zakzakst <e1433104@gmai.com>
2025-04-16 16:45:35 +0900

    修正

---
+++ src/components/molecules/TextboxWithInfo/index.test.tsx
@@ -0,0 +1,31 @@
+import { composeStories } from "@storybook/react";
+import { render, screen } from "@testing-library/react";
+import { TextboxWithInfo } from ".";
+import * as stories from "./index.stories";
+
+const { FullProps } = composeStories(stories);
+
+test("TextboxWithInfo", () => {
+  render(<FullProps />);
+  expect(screen.getByRole("textbox")).toHaveAccessibleName("記事タイトル");
+  expect(screen.getByRole("textbox")).toHaveAccessibleDescription(
+    "半角英数64文字以内で入力してください"
+  );
+  expect(screen.getByRole("textbox")).toHaveAccessibleErrorMessage(
+    "不正な文字が含まれています"
+  );
+});
+
+test("TextboxWithInfo", () => {
+  const args = {
+    title: "記事タイトル",
+    info: "0 / 64",
+    description: "半角英数64文字以内で入力してください",
+    error: "不正な文字が含まれています",
+  };
+  render(<TextboxWithInfo {...args} />);
+  const textbox = screen.getByRole("textbox");
+  expect(textbox).toHaveAccessibleName(args.title);
+  expect(textbox).toHaveAccessibleDescription(args.description);
+  expect(textbox).toHaveAccessibleErrorMessage(args.error);
+});
