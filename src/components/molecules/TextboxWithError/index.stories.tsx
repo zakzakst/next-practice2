83d73903489c8dcc0c3ca45b60cb5edc0f18df56
zakzakst <e1433104@gmai.com>
2025-04-16 14:58:33 +0900

    修正

---
+++ src/components/molecules/TextboxWithError/index.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { TextboxWithError } from ".";
+
+const meta = {
+  title: "Molecules/TextboxWithError",
+  component: TextboxWithError,
+  args: {
+    id: "title",
+    name: "title",
+    defaultValue: "タイトル",
+    maxLength: 10,
+  },
+  // TODO: a11y設定する
+  // parameters: {
+  //   a11y: {
+  //     config: { rules: [{ id: "label", enabled: false }] },
+  //   },
+  // },
+} satisfies Meta<typeof TextboxWithError>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Error: Story = {
+  args: {
+    error: "エラーがあります",
+  },
+};
