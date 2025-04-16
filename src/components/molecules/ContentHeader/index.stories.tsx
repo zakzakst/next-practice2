7b93c28a7af6cbc855f953beef224d3a5bec0084
zakzakst <e1433104@gmai.com>
2025-04-16 13:36:43 +0900

    修正

---
+++ src/components/molecules/ContentHeader/index.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { ContentHeader } from ".";
+
+const meta = {
+  title: "Molecules/ContentHeader",
+  component: ContentHeader,
+} satisfies Meta<typeof ContentHeader>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: { title: "見出し", description: "概要" },
+};
