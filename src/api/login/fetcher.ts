c299e9eb85674a124f51459ba3c9c616bb2df548
zakzakst <e1433104@gmai.com>
2025-04-17 13:12:39 +0900

    修正

--- src/api/login/fetcher.ts
+++ src/api/login/fetcher.ts
@@ -1,11 +1,14 @@
 import { defaultHeaders, host } from "..";
 import { PostLoginRequest, PostLoginResponse, PostLoginError } from "./type";
+import { PostLoginResponseMock } from "@/mocks/login";
 
 const path = () => host("/login");
 
 export const postLogin = async (
   request: PostLoginRequest
 ): Promise<PostLoginResponse> => {
+  if (process.env.NEXT_PUBLIC_FEATURE_ABLE_API === "false")
+    return PostLoginResponseMock;
   try {
     const res = await fetch(path(), {
       method: "POST",
