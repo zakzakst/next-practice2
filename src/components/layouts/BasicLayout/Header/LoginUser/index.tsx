6ecdc483761db256a0d2f64b911fdf2bd5e7fcaf
zakzakst <e1433104@gmai.com>
2025-04-19 14:38:27 +0900

    修正

--- src/components/layouts/BasicLayout/Header/LoginUser/index.tsx
+++ src/components/layouts/BasicLayout/Header/LoginUser/index.tsx
@@ -1,1 +1,40 @@
-// import { Button } from "@/components/atoms/Button";
+import { Button } from "@/components/atoms/Button";
+import { postLogout } from "@/api/logout";
+import { GetMyProfileResponse } from "@/api/myProfile";
+import styles from "./styles.module.css";
+
+type Props = GetMyProfileResponse;
+
+export const LoginUser = ({ name, imageUrl }: Props) => {
+  const handleLogout = async () => {
+    try {
+      await postLogout();
+      window.location.reload();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <section
+      role="region"
+      aria-label="ログインユーザー"
+      className={styles.user}
+    >
+      <p className={styles.userName}>{name}</p>
+      <p
+        className={styles.thumbnail}
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className={styles.logout}>
+        <div className={styles.logoutInner}>
+          <div className={styles.logoutBox}>
+            <Button theme="transparent" variant="small" onClick={handleLogout}>
+              ログアウト
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
