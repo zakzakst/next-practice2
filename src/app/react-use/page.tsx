b871af5c94cf24c097a3ae3aa8f58a1951cc507e
zakzakst <e1433104@gmai.com>
2025-04-28 20:36:27 +0900

    修正

--- src/app/react-use/page.tsx
+++ src/app/react-use/page.tsx
@@ -1,12 +1,13 @@
 "use client";
-// https://github.com/streamich/react-use/blob/HEAD/docs/useHash.md
 // https://github.com/streamich/react-use/blob/HEAD/docs/useIntersection.md
 import { UseHoverTest } from "./components/UseHoverTest";
+import { UseHashTest } from "./components/UseHashTest";
 
 const Page = () => {
   return (
     <div>
       <UseHoverTest />
+      <UseHashTest />
     </div>
   );
 };
